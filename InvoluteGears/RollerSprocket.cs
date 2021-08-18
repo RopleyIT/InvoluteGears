edfaf7c2028e257f7a54ae06f9ac7a353ae8bfac
Sean D Smith <sdsmith@ropley.com>
2021-08-18 11:42:52 +0100

    Roller sprocket teeth trnucated at roller outer diameter

--- InvoluteGears/RollerSprocket.cs
+++ InvoluteGears/RollerSprocket.cs
@@ -47,19 +47,19 @@ namespace InvoluteGears
         /// <summary>
         /// The linear distance between adjacent roller centres
         /// </summary>
-        
+
         public double Pitch { get; private set; }
 
         /// <summary>
         /// The diameter of the rollers in the chain
         /// </summary>
-        
+
         public double RollerDiameter { get; private set; }
 
         /// <summary>
         /// The desired slack between the rollers and the sprocket
         /// </summary>
-        
+
         public double Backlash { get; private set; }
 
         /// <summary>
@@ -69,9 +69,9 @@ namespace InvoluteGears
         /// opposite roller centres divided by the number of teeth on
         /// the sprocket.
         /// </summary>
-        
+
         public double Module =>
-            Pitch / (ToothCount * Math.Sin(Math.PI / ToothCount));
+            2 * PitchRadius / ToothCount;
 
         /// <summary>
         /// The tolerance in precision for cutting the sprocket
@@ -98,14 +98,21 @@ namespace InvoluteGears
         /// The diameter of the base circle on which the chain edges
         /// rest when wrapped around the sprocket
         /// </summary>
-        
+
         public double InnerDiameter =>
-            Pitch/Math.Sin(Math.PI/ToothCount) - ChainWidth;
+            2 * PitchRadius - ChainWidth;
+
+        /// <summary>
+        /// Radius of sprocket to centre of chain roller
+        /// </summary>
+        
+        public double PitchRadius =>
+            Pitch / (2 * Math.Sin(Math.PI / ToothCount));
 
         /// <summary>
         /// The dimension for the size of the rendered image
         /// </summary>
-        
+
         public double OuterDiameter { get; private set; }
 
         private List<PointF> CalculateOneTooth()
@@ -137,7 +144,8 @@ namespace InvoluteGears
             // The angle from the roller centre to the tip of
             // the tooth at the sprocket's maximum radius
 
-            var tipAngle = Math.Asin(jy / (Pitch - rollerRadius));
+            var addendumAngle = AddendumAngle();
+            var tipAngle = ToothCurveAngle(addendumAngle);
 
             // Calculate the part of the tooth that clears the
             // roller while it is unwinding from the sprocket
@@ -149,13 +157,20 @@ namespace InvoluteGears
                 Pitch - rollerRadius,
                 Involutes.CreatePt(jx, -jy));
 
+            var rimPoints = Involutes.CirclePoints(
+                0,
+                addendumAngle,
+                Involutes.AngleStep,
+                PitchRadius + rollerRadius);
+
             // Set the dimensions of the bounding box
 
-            OuterDiameter = clearPts.First().X * 2;
-
+            OuterDiameter = (PitchRadius + rollerRadius) * 2;
             return contactPts
-                .Select(p => new PointF(p.X, -p.Y))
-                .Concat(clearPts.Reverse().Select(p => new PointF(p.X, -p.Y)))
+                .Select(p => Involutes.Conjugate(p))
+                .Concat(clearPts.Reverse().Select(p => Involutes.Conjugate(p)))
+                .Concat(rimPoints.Select(p => Involutes.Conjugate(p)).Reverse())
+                .Concat(rimPoints)
                 .Concat(clearPts)
                 .Concat(contactPts.Reverse()).ToList();
         }
@@ -184,9 +199,56 @@ namespace InvoluteGears
         /// Calculate the path for the base wheel either side of the teeth
         /// </summary>
         /// <returns>Base wheel circle</returns>
-        
-        public IEnumerable<PointF> GenerateInnerGearPath() => 
+
+        public IEnumerable<PointF> GenerateInnerGearPath() =>
             Involutes.CirclePoints
                 (0, 2 * Math.PI, Involutes.AngleStep, InnerDiameter / 2);
+
+        /// <summary>
+        /// Calculate the angle between the X axis and the point at which the
+        /// addendum circle meets the tooth edge. This enables us to cut off
+        /// the teeth rather than have a long sharp point sticking out past
+        /// the outer edge of the roller chain.
+        /// </summary>
+        /// <returns>The angle between the X axis and the point at which
+        /// the addendum circle meets the tooth edge</returns>
+
+        private double AddendumAngle()
+        {
+            var rollerRadius = (RollerDiameter + Backlash) / 2;
+            var addendumRadius = PitchRadius + rollerRadius;
+            var toothRadius = Pitch - rollerRadius;
+            var cosCentreAngle = PitchRadius * PitchRadius 
+                + addendumRadius * addendumRadius 
+                - toothRadius * toothRadius;
+            cosCentreAngle /= 2 * PitchRadius * addendumRadius;
+            return Math.Acos(cosCentreAngle) - Math.PI / ToothCount;
+        }
+
+        /// <summary>
+        /// Obtain the angle formed from the centre of the lower roller
+        /// to the point at which the tooth curve meets the addendum circle
+        /// </summary>
+        /// <param name="addendumAngle">The addendum angle as computed
+        /// by calling AddendumAngle()</param>
+        /// <returns>The angle to the interssection point</returns>
+        
+        private double ToothCurveAngle(double addendumAngle)
+        {
+            // Find the centre of the lower roller
+
+            var rx = Math.Sqrt(PitchRadius*PitchRadius - Pitch*Pitch/4);
+            var ry = -Pitch / 2;
+
+            // Find the intersection point coordinates
+
+            var rollerRadius = (RollerDiameter + Backlash) / 2;
+            var mx = (PitchRadius + rollerRadius) * Math.Cos(addendumAngle);
+            var my = (PitchRadius + rollerRadius) * Math.Sin(addendumAngle);
+
+            // Calculate the angle from (rx, ry) to (mx, my)
+
+            return Math.Atan2(my - ry, mx - rx);
+        }
     }
 }
