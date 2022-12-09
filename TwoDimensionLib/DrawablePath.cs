ddd0b3952ace7ea66ebef54d157c65938279cd17
Sean D Smith <sdsmith@ropley.com>
2022-12-09 10:53:46 +0000

    Made the compound DrawablePath implement IDrawable

--- TwoDimensionLib/DrawablePath.cs
+++ TwoDimensionLib/DrawablePath.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TwoDimensionLib
 {
-    public class DrawablePath
+    public class DrawablePath : IDrawable
     {
         public IList<IDrawable> Curves { get; set; }
         public bool Closed { get; set; }
@@ -21,5 +21,41 @@ namespace TwoDimensionLib
                 return b.Bounds;
             }
         }
+
+        public Coordinate Start => Curves[0].Start;
+
+        public Coordinate End => Curves[Curves.Count - 1].End;
+
+        public IDrawable ReflectY()
+            => new DrawablePath
+            {
+                Curves = new List<IDrawable>
+                    (this.Curves.Select(c => c.ReflectY())),
+                Closed = this.Closed
+            };
+
+        public IDrawable Reversed()
+            => new DrawablePath
+            {
+                Curves = new List<IDrawable>
+                    (this.Curves.Reverse().Select(c => c.Reversed())),
+                Closed = this.Closed
+            };
+
+        public IDrawable RotatedBy(double phi, Coordinate pivot)
+            => new DrawablePath
+            {
+                Curves = new List<IDrawable>
+                    (this.Curves.Select(c => c.RotatedBy(phi, pivot))),
+                Closed = this.Closed
+            };
+
+        public IDrawable Translated(Coordinate offset)
+            => new DrawablePath
+            {
+                Curves = new List<IDrawable>
+                    (this.Curves.Select(c => c.Translated(offset))),
+                Closed = this.Closed
+            };
     }
 }
